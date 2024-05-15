e394fb6063b2a557b91899e03355fafddc8c8fed
Dennis Chen <70829793@qq.com>
2024-05-15 15:07:30 +0800

    fix windows path issue

--- src/removeRootPath.js
+++ src/removeRootPath.js
@@ -2,7 +2,7 @@ import path from 'path'
 
 export default (root, file) => {
   let result = path.normalize(file)
-  result = file.replace(path.normalize(root), '')
+  result = result.replace(path.normalize(root), '')
   result = result.replace(/\\/g, '/')
 
   // remove leading slash
